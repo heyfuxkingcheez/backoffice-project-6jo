bee268f3f2f846e1f0de2124c490c2a5312e02e6
halbebe <gksmfsf97@naver.com>
2023-12-13 12:35:17 +0900

    [delete] users.router에 있는 괘씸한 로그인 지웠습니다

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -6,6 +6,4 @@ const userController = new UserController();
 
 userRouter.post("/signup", userController.signup); //회원가입
 
-// authRouter.post("/signin", authController.signin); //로그인
-
 export { userRouter };
