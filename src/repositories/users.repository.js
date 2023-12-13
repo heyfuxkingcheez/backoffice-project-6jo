3dc4735e454c82324979a0af4fcbe21fe56c599f
halbebe <gksmfsf97@naver.com>
2023-12-13 11:51:08 +0900

    [ADD] users.repository.js 코드 추가

--- src/repositories/users.repository.js
+++ src/repositories/users.repository.js
@@ -1,1 +1,25 @@
-export class UsersRepository {}
+export class UsersRepository {
+  createOne = async ({ email, password, nickname }) => {
+    const hashedPassword = bcrypt.hashSync(password, PASSWORD_HASH_SALT_ROUNDS);
+
+    const newUser = await prisma.users.create({
+      data: { email, password: hashedPassword, nickname },
+    });
+
+    delete newUser.password;
+
+    return newUser;
+  };
+
+  readOneById = async (id) => {
+    const user = await prisma.users.findUnique({ where: { id } });
+
+    return user;
+  };
+
+  readOneByEmail = async (email) => {
+    const user = await prisma.users.findUnique({ where: { email } });
+
+    return user;
+  };
+}
