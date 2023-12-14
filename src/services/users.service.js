9183e3ea5ca658e8bac573fe42b698d5dcf16c7d
halbebe <gksmfsf97@naver.com>
2023-12-14 15:36:57 +0900

    [ADD] 이메일 중복확인 아직 구현 못함

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,10 +1,3 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-// import {
-//   JWT_ACCESS_TOKEN_SECRET,
-//   JWT_ACCESS_TOKEN_EXPIRES_IN,
-// } from "../constants/security.costant.js";
-// import * as HttpStatus from "../errors/http-status.error.js";
 import { UsersRepository } from "../repositories/users.repository.js";
 
 export class UsersService {
@@ -13,10 +6,12 @@ export class UsersService {
   }
 
   signup = async (email, nickname, password, role) => {
-    const existedUser = await this.usersRepository.readOneByEmail(email);
-
-    if (existedUser) {
-      throw new HttpStatus.BadRequest("이미 가입 된 이메일입니다.");
+    const existsUserEmail = await this.usersRepository.readOneByEmail(email);
+    if (existsUserEmail) {
+      return res.status(405).json({
+        success: false,
+        message: "이미 가입 된 이메일",
+      });
     }
 
     const newUser = await this.usersRepository.createOne(
@@ -26,25 +21,10 @@ export class UsersService {
       role
     );
 
-    return newUser;
+    return {
+      success: true,
+      message: "가입 성공",
+      data: newUser,
+    };
   };
-
-  // signin = async ({ email, password }) => {
-  //   const user = await this.usersRepository.readOneByEmail(email);
-  //   const hashedPassword = user?.password ?? "";
-
-  //   const isPasswordMatched = bcrypt.compareSync(password, hashedPassword);
-
-  //   const isCorrectUser = user && isPasswordMatched;
-
-  //   if (!isCorrectUser) {
-  //     throw new HttpStatus.Unauthorized("일치하는 인증 정보가 없습니다.");
-  //   }
-
-  //   const accessToken = jwt.sign({ userId: user.id }, process.env.TOKENKEY, {
-  //     expiresIn: process.env.JWT_EXPIRE,
-  //   });
-
-  //   return accessToken;
-  // };
 }
