fe4e409b668094390fa3b4bc4b6e91829cc99836
woogi-on_and-on <jgw987@gmail.com>
2023-12-15 03:16:46 +0900

    feat [dev] 주문 결제 - transaction 적용

--- src/repositories/order.repository.js
+++ src/repositories/order.repository.js
@@ -1,40 +1,76 @@
+import { PrismaClientValidationError } from "@prisma/client/runtime/library.js";
 import { prisma } from "../utils/prisma/index.js";
 
 export class OrderRepository {
   // 잔고 확인
   checkPoint = async (userId) => {
-    const checkedPoint = await prisma.point.findFirst({
-      where: { UserId: userId },
-    });
-    return checkedPoint;
-  };
-
-  // 잔고 수정
-  createPoint = async (userId, totalPrice) => {
     const checkedPoint = await prisma.point.findMany({
       where: { UserId: userId },
       orderBy: { createdAt: "desc" },
       take: 1,
     });
-    console.log("checkedPoint: ", checkedPoint[0].balance);
-    const createdPoint = await prisma.point.create({
-      data: {
-        UserId: userId,
-        income: 0,
-        expense: totalPrice,
-        balance: checkedPoint[0].balance - totalPrice,
-      },
-    });
-    return createdPoint;
+    return checkedPoint;
   };
 
-  // 주문 생성
-  createOrder = async (userId, MenuId, orderDetails, totalPrice) => {
-    console.log("MenuId: ", MenuId);
-    console.log("userId: ", userId);
-    const createdOrder = await prisma.orders.createMany({
-      data: { MenuId, UserId: userId, orderDetails, totalPrice },
-    });
-    return createdOrder;
+  //------------------------------------------------------------
+  // // 잔고 수정
+  // createPoint = async (userId, totalPrice) => {
+  //   const checkedPoint = await prisma.point.findMany({
+  //     where: { UserId: userId },
+  //     orderBy: { createdAt: "desc" },
+  //     take: 1,
+  //   });
+  //   console.log("checkedPoint: ", checkedPoint[0].balance);
+  //   const createdPoint = await prisma.point.create({
+  //     data: {
+  //       UserId: userId,
+  //       income: 0,
+  //       expense: totalPrice,
+  //       balance: checkedPoint[0].balance - totalPrice,
+  //     },
+  //   });
+  //   return createdPoint;
+  // };
+
+  // // 주문 생성
+  // createOrder = async (userId, MenuId, orderDetails, totalPrice) => {
+  //   console.log("MenuId: ", MenuId);
+  //   console.log("userId: ", userId);
+  //   const createdOrder = await prisma.orders.createMany({
+  //     data: { MenuId, UserId: userId, orderDetails, totalPrice },
+  //   });
+  //   return createdOrder;
+  // };
+  //-----------------------------------------------------------
+
+  // 주문 결제 Transaction 적용
+  orderPayment = async (userId, MenuId, orderDetails, totalPrice) => {
+    const [createdPoint, createdOrder] = await prisma.$transaction(
+      async (tx) => {
+        const checkedPoint = await tx.point.findMany({
+          where: { UserId: userId },
+          orderBy: { createdAt: "desc" },
+          take: 1,
+        });
+        // console.log("checkedPoint: ", checkedPoint[0].balance);
+        if (checkedPoint[0].balance <= 0) throw new Error("잔액 부족");
+
+        const createdPoint = await tx.point.create({
+          data: {
+            UserId: userId,
+            income: 0,
+            expense: totalPrice,
+            balance: checkedPoint[0].balance - totalPrice,
+          },
+        });
+
+        const createdOrder = await tx.orders.create({
+          data: { MenuId, UserId: userId, orderDetails, totalPrice },
+        });
+        return [createdPoint, createdOrder];
+      }
+    );
+
+    return [createdPoint, createdOrder];
   };
 }
--- src/services/order.service.js
+++ src/services/order.service.js
@@ -7,35 +7,53 @@ export class OrderService {
   checkPoint = async (userId) => {
     const point = await this.orderRepository.checkPoint(userId);
     return {
-      userId: point.userId,
+      userId: point.UserId,
       balance: point.balance,
     };
   };
 
   // 주문 생성
-  createOrder = async (userId, MenuId, orderDetails, totalPrice) => {
-    const point = await this.orderRepository.checkPoint(userId);
+  // createOrder = async (userId, MenuId, orderDetails, totalPrice) => {
+  //   const point = await this.orderRepository.checkPoint(userId);
 
-    if (point.balance < totalPrice) throw new Error("잔액이 부족합니다.");
+  //   if (!point || point.balance < totalPrice)
+  //     throw new Error("잔액이 부족합니다.");
 
-    const createdOrder = await this.orderRepository.createOrder(
+  //   const createdOrder = await this.orderRepository.createOrder(
+  //     userId,
+  //     MenuId,
+  //     orderDetails,
+  //     totalPrice
+  //   );
+  //   const createdPoint = await this.orderRepository.createPoint(
+  //     userId,
+  //     totalPrice,
+  //     point.pointId
+  //   );
+
+  //   return {
+  //     userId: createdOrder.userId,
+  //     MenuId: createdOrder.MenuId,
+  //     orderDetails: createdOrder.orderDetails,
+  //     totalPrice: createdOrder.totalPrice,
+  //     balance: createdPoint.balance,
+  //   };
+  // };
+
+  // 주문 결제
+  orderPayment = async (userId, MenuId, orderDetails, totalPrice) => {
+    // const checkedPoint = await this.orderRepository.checkPoint(userId);
+    // console.log(checkedPoint);
+    // if (checkedPoint[0].balance <= 0) throw new Error("잔액 부족");
+
+    const orderPayment = await this.orderRepository.orderPayment(
       userId,
       MenuId,
       orderDetails,
       totalPrice
     );
-    const createdPoint = await this.orderRepository.createPoint(
-      userId,
-      totalPrice,
-      point.pointId
-    );
+    // console.log("orderPayment: ", orderPayment);
 
-    return {
-      userId: createdOrder.userId,
-      MenuId: createdOrder.MenuId,
-      orderDetails: createdOrder.orderDetails,
-      totalPrice: createdOrder.totalPrice,
-      balance: createdPoint.balance,
-    };
+    return orderPayment;
   };
 }
