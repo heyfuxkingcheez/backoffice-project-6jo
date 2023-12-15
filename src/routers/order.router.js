7b2923348304cea8749adae6a39d5aaf237e9a1c
woogi-on_and-on <jgw987@gmail.com>
2023-12-15 15:08:33 +0900

    feat [order] 사장님 미들웨어 추가 로그인한 계정 -> 사업장을 가지고 있는지 check 사업장을 가지고 있다면 -> params에 접근한 restaurantId와 동일한지 check

--- src/routers/order.router.js
+++ src/routers/order.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { auth_middleware } from "../middlewares/auth.middleware.js";
 import { OrderController } from "../controllers/order.controller.js";
+import { auth_owner_middleware } from "../middlewares/auth.owner.middleware.js";
 
 const orderRouter = Router();
 const orderController = new OrderController();
@@ -15,12 +16,14 @@ orderRouter.post(
 orderRouter.get(
   "/:restaurantId/order",
   auth_middleware,
+  auth_owner_middleware,
   orderController.getOrders
 );
 // // 주문 상태 수정 API(배달 완료) (사장)
 orderRouter.patch(
   "/:restaurantId/order/:orderId",
   auth_middleware,
+  auth_owner_middleware,
   orderController.completeOrder
 );
 
